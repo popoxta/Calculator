2e9d08647b78b1cbc5f98a68d6a3606803f49955
popoxta <popoxta@gmail.com>
2023-02-07 15:49:28 +1300

    add keyboard support and fixed multiple decimal points

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ let currentNumber = ""
 
 //display values
 const display = document.getElementById('display')
+
 function showValues() {
     display.textContent = `${storedNumber} ${operator} ${currentNumber}`
     console.log(`stored: ${storedNumber} operator: ${operator} current: ${currentNumber}`)
@@ -12,7 +13,7 @@ function showValues() {
 
 //clear all values andd display
 const clearAll = document.getElementById('clear')
-clearAll.addEventListener("click", ()=>{
+clearAll.addEventListener("click", () => {
     storedNumber = ""
     operator = ""
     currentNumber = ""
@@ -21,43 +22,61 @@ clearAll.addEventListener("click", ()=>{
 
 //numbers pad and input
 const numPad = document.getElementById('calculator-num')
-numPad.addEventListener('click', (e) => { //UPDATES CURRENT NUMBER
-    const clicked = e.target.id
-    if (clicked === "calculator-num") {
+numPad.addEventListener('click', (e) => numberPressed(e.target.id))
+
+function numberPressed(eventTargetId) {
+    const clicked = eventTargetId
+    if (clicked === "calculator-num" || currentNumber.indexOf('.') !== -1 && clicked === ".") {
         return;
-    } else if (operator === "" && storedNumber !== "") {
+    }
+    if (operator === "" && storedNumber !== "") {
         storedNumber = ""
     }
     display.textContent += clicked
     currentNumber += clicked
     showValues()
-})
+}
 
 //operators pad and input
 const operatorPad = document.getElementById('operators')
-operatorPad.addEventListener("click", (e) => {
-    const clicked = e.target.id
+operatorPad.addEventListener("click", (e) => operatorPressed(e.target.id))
+
+function operatorPressed(eventTargetId) {
+    const clicked = eventTargetId
     if (clicked === "operators" || currentNumber === "" && storedNumber === "") {
         return
     }
-
     if (currentNumber !== "" && storedNumber === "") {
         storedNumber = currentNumber
         currentNumber = ""
     }
     operator = clicked
     showValues()
-})
+}
 
 //calculation of results and continuation
 const calculateBtn = document.getElementById('calculate')
-calculateBtn.addEventListener('click', () => {
-    const result = calculateNumber(+storedNumber, +currentNumber, operator)
-    storedNumber = Math.round(result * 100) / 100
+calculateBtn.addEventListener('click', calculateResult)
+
+function calculateResult() {
+    if (storedNumber === "" || currentNumber === "" || operator === "") {
+        return
+    }
+
+    const result = Math.round(calculateNumber(+storedNumber, +currentNumber, operator) * 100) / 100
+
+    if (!isFinite(result)) {
+        display.textContent = "Very funny, how clever."
+        storedNumber = ""
+        operator = ""
+        currentNumber = ""
+        return;
+    }
+    storedNumber = result
+    display.textContent = storedNumber
     currentNumber = ""
     operator = ""
-    display.textContent = result
-})
+}
 
 function calculateNumber(num1, num2, operator) {
     const operations = {
@@ -68,3 +87,17 @@ function calculateNumber(num1, num2, operator) {
     }
     return operations[operator]
 }
+
+//keypress support
+document.addEventListener("keypress", (e) => {
+    e.preventDefault()
+    console.log(e)
+    if (e.code.indexOf('Digit') !== -1 || e.key === ".") {
+        numberPressed(e.key)
+    } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*"
+    ) {
+        operatorPressed(e.key)
+    } else if (e.key === "=" || e.key === "Enter") {
+        calculateResult()
+    }
+})
