c894d7def79d56d06ae9a7c5dc7da7b77da5c233
popoxta <popoxta@gmail.com>
2023-02-07 14:45:17 +1300

    implement functionality for basic math operations

--- scripts.js
+++ scripts.js
@@ -1,32 +1,70 @@
+let storedNumber = ""
+let operator = ""
+let currentNumber = ""
+
+
+//display values
+const display = document.getElementById('display')
+function showValues() {
+    display.textContent = `${storedNumber} ${operator} ${currentNumber}`
+    console.log(`stored: ${storedNumber} operator: ${operator} current: ${currentNumber}`)
+}
+
+//clear all values andd display
+const clearAll = document.getElementById('clear')
+clearAll.addEventListener("click", ()=>{
+    storedNumber = ""
+    operator = ""
+    currentNumber = ""
+    display.textContent = ""
+})
+
+//numbers pad and input
+const numPad = document.getElementById('calculator-num')
+numPad.addEventListener('click', (e) => { //UPDATES CURRENT NUMBER
+    const clicked = e.target.id
+    if (clicked === "calculator-num") {
+        return;
+    } else if (operator === "" && storedNumber !== "") {
+        storedNumber = ""
+    }
+    display.textContent += clicked
+    currentNumber += clicked
+    showValues()
+})
+
+//operators pad and input
+const operatorPad = document.getElementById('operators')
+operatorPad.addEventListener("click", (e) => {
+    const clicked = e.target.id
+    if (clicked === "operators" || currentNumber === "" && storedNumber === "") {
+        return
+    }
+
+    if (currentNumber !== "" && storedNumber === "") {
+        storedNumber = currentNumber
+        currentNumber = ""
+    }
+    operator = clicked
+    showValues()
+})
+
+//calculation of results and continuation
+const calculateBtn = document.getElementById('calculate')
+calculateBtn.addEventListener('click', () => {
+    const result = calculateNumber(+storedNumber, +currentNumber, operator)
+    storedNumber = Math.round(result * 100) / 100
+    currentNumber = ""
+    operator = ""
+    display.textContent = result
+})
 
 function calculateNumber(num1, num2, operator) {
     const operations = {
-        add: num1 + num2,
-        subtract: num1 - num2,
-        multiply: num1 * num2,
-        divide: num1 / num2
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        '/': num1 / num2
     }
     return operations[operator]
 }
-
-calculateNumber(1, 1, "add")
-
-
-
-// CLEAR
-const clearAll = document.getElementById('clear')
-clearAll.addEventListener('click', () => displayBox.textContent = "")
-
-//CALCULATOR MAIN
-    //DISPLAY BOX
-const displayBox = document.getElementById('display')
-const calculatorMain = document.getElementById('calculator-main')
-calculatorMain.addEventListener('click', (e) => {
-    let clicked = e.target.id
-    if (clicked === "calculator-main") {
-        return
-    }
-    displayBox.textContent+=clicked
-})
-
-//OPERATE
